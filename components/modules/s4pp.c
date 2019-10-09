0c9844fd02724f2942acbe0ffa1842dcf3a59d26
Johny Mattsson <jmattsson@dius.com.au>
2019-10-09 18:09:26 +1100

    s4pp: actually report more network errors
    By the time io_disconnect() is called from the s4pp_client context, we've
    already torn down our state and thus wouldn't get the error report.

--- components/modules/s4pp.c
+++ components/modules/s4pp.c
@@ -392,10 +392,10 @@ static void handle_conn(task_param_t param, task_prio_t prio)
   if (!conn || !conn_is_active(conn)) // active check needed for dns case
     goto done;
 
-  if (nbe->evt == CONN_EVT_ERR)
-    goto err;
-
   s4pp_state_t *state = state_by_conn(conn);
+  if (nbe->evt == CONN_EVT_ERR)
+    goto network_err;
+
   if (!state || !state->ctx)
     goto err;
 
@@ -406,7 +406,7 @@ static void handle_conn(task_param_t param, task_prio_t prio)
       {
         ESP_LOGE("s4pp", "excessive netconn send events, %d vs %d",
           nbe->len, conn->left_to_send);
-        goto err;
+        goto network_err;
       }
       conn->left_to_send -= nbe->len;
       if (nbe->len && conn->left_to_send == 0)
@@ -422,7 +422,7 @@ static void handle_conn(task_param_t param, task_prio_t prio)
       struct netbuf *nb = NULL;
       err_t res = netconn_recv(conn->netconn, &nb);
       if (res != ERR_OK || !nb)
-        goto err;
+        goto network_err;
       netbuf_first(nb);
       do {
         void *payload;
@@ -442,17 +442,9 @@ static void handle_conn(task_param_t param, task_prio_t prio)
         {
           err_t err = dns_gethostbyname(state->server.hostname, &conn->resolved_ip, dns_resolved, conn);
           if (err == ERR_OK)
-          {
             dns_resolved(state->server.hostname, &conn->resolved_ip, conn);
-          }
-          else if (err == ERR_INPROGRESS)
-          {
-          }
-          else
-          {
-            report_error(lua_getstate(), state, S4PP_NETWORK_ERROR);
-            goto err;
-          }
+          else if (err != ERR_INPROGRESS)
+            goto network_err;
         }
         else
         {
@@ -465,7 +457,7 @@ static void handle_conn(task_param_t param, task_prio_t prio)
         conn->resolved_ip = nbe->addr;
         conn->netconn = netconn_new_with_callback(NETCONN_TCP, on_netconn_evt);
         if (!conn->netconn)
-          goto err;
+          goto network_err;
         netconn_set_nonblocking(conn->netconn, 1);
         ip_set_option(conn->netconn->pcb.tcp, SOF_KEEPALIVE);
 
@@ -477,6 +469,8 @@ static void handle_conn(task_param_t param, task_prio_t prio)
     default: break;
   }
   goto done;
+network_err:
+  report_error(lua_getstate(), state, S4PP_NETWORK_ERROR);
 err:
   free_connection(conn);
 done:
