d0cd08bf940b96b2a5b733e243a8cf9f98039c01
Bernd Meyer <bmeyer@dius.com.au>
2021-11-06 12:16:35 +1100

    Change UART fifo threshold, to avoid overruns

--- components/platform/platform.c
+++ components/platform/platform.c
@@ -320,6 +320,11 @@ int platform_uart_start( unsigned id )
     return -1;
   }
 
+  // The default threshold of 120 seems a tad aggressive, and we occasionally get overruns
+  // while flash operations are happening. No need to error-check this; It's a best-effort
+  // attempt, if it fails, too bad.
+  uart_set_rx_full_threshold(id,100);
+
   char pcName[6];
   snprintf( pcName, 6, "uart%d", id );
   pcName[5] = '\0';
