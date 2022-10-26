2053a09e6f41f0a44da6679af24e14ac6be74084
Bernd Meyer <bmeyer@dius.com.au>
2022-10-26 15:07:45 +1030

    Fix issue where LFS embedded failed for certain sizes on RISCV

--- tools/embed_lfs.sh
+++ tools/embed_lfs.sh
@@ -18,9 +18,15 @@ fi
 # Extract the line containing the data size of the LFS object, filtering out
 # lines for .bss/.data/.text, sorting the remaining two entries (actual LFS
 # data and (optional) riscv attributes) so we can discard the latter if
-# present. If the map file was a bit saner with its line breaks this would
+# present.
+# Unfortunately, the sort-by-size does not work as intended, because the
+# sizes are in hex, and sort -n thinks 0x2d004 is less than 0x24.
+# As a fix, we instead (or rather, prior to attempting the sort)
+# filter out the line containing the word "0x24", which appears to be the
+# (fixed) size of riscv attributes.
+# If the map file was a bit saner with its line breaks this would
 # have been a straight forward grep for for .rodata.embedded.*lua.flash.store
-LFS_SIZE_ADDR=$(grep -E "0x[0-9a-f]+[ ]+0x[0-9a-f]+[ ]+esp-idf/embedded_lfs/libembedded_lfs.a\(lua.flash.store.reserved.S.obj\)" "${MAP_FILE}" | grep -v '^ \.' | awk '{print $2,$1}' | sort -n -k 1.3 | tail -1)
+LFS_SIZE_ADDR=$(grep -E "0x[0-9a-f]+[ ]+0x[0-9a-f]+[ ]+esp-idf/embedded_lfs/libembedded_lfs.a\(lua.flash.store.reserved.S.obj\)" "${MAP_FILE}" | grep -v '^ \.' | grep -vw "0x24" | awk '{print $2,$1}' | sort -n -k 1.3 | tail -1)
 if [ -z "${LFS_SIZE_ADDR}" ]; then
 	echo "Error: LFS segment not found. Use 'make clean; make' perhaps?"
 	exit 1
